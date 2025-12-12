307bbcd1af9e9a91b02d8e0d3ac7d81974f457a0
dranel1441 <binglejohndangangao@gmail.com>
2025-12-12 13:02:54 +0800

    Last na to

--- bingle_portfolio/routes/web.php
+++ bingle_portfolio/routes/web.php
@@ -1,6 +1,21 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
-use App\Http\Controllers\mainController;
-route::get('/', [mainController::class, 'index']);
+Route::get('/', function () {
+    return view('welcome');
+});
+// Route::view(Portfolio, Portfolio);
+Route::get('/', function () {
+    return view('Portfolio');
+});
+Route::get('/register', [Authcontroller::class, 'showRegister'])->name('register.form');
+Route::post('/register', [Authcontroller::class, 'Register'])->name('register');
+
+Route::get('/login', [Authcontroller::class, 'showLogin'])->name('login.form');
+Route::post('/login', [Authcontroller::class, 'performLogin'])->name('login');
+
+Route::post('/logout', function(){
+
+});
